b1453dd9ae520c345c78b72beca1d74fdcdd8d9c
Crixxee <105209469+Crixxee@users.noreply.github.com>
2022-07-02 13:56:37 -0700

    Uploaded file
    This is my own contribution to this project on zuri-get-practice.
    It is my first practice on fork, clone and pull request (contributing to another person's project).

---
+++ Crixxy.js
@@ -0,0 +1,7 @@
+// This project is done in response to zuri git practice, set up by Dapo Adedire on slack, to practice how to contribute to another person's project on GitHub (fork, clone and pull request). 
+
+console.log('Name: Eke Adaobi C')
+console.log('Slack username: Crixxy');
+console.log('Nationality: Nigerian');
+console.log('Device: Phone');
+console.log('Appreciation: I want to say a very big thank you to everyone that is making this journey with zuri successful.');
